48ab1b8d66b72715d0e630139008cb6360765ad0
spikepanx <spikepanx@wwsjd.de>
2022-01-26 07:45:23 +0100

    ...

--- internal/bin/statestore.go
+++ internal/bin/statestore.go
@@ -1,4 +1,4 @@
-package main
+package bin
 
 /*
 	incoming messages are persisted as k, v
@@ -8,18 +8,14 @@ package main
 */
 
 import (
-	"flag"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
-	"github.com/boltdb/bolt"
 	"github.com/golang/glog"
 )
 
-func sigHandler() <-chan struct{} {
+func SigHandler() <-chan struct{} {
 	termCh := make(chan struct{})
 	go func() {
 		c := make(chan os.Signal, 1)
@@ -37,30 +33,30 @@ func sigHandler() <-chan struct{} {
 	return termCh
 }
 
-func loadConfig() {
-	flag.Parse()
-}
+// func loadConfig() {
+// 	flag.Parse()
+// }
 
-var (
-	termCh = sigHandler()
-	db     *bolt.DB
-)
+// var (
+// 	termCh = sigHandler()
+// 	db     *bolt.DB
+// )
 
-func main() {
-	var err error
+// func main() {
+// 	var err error
 
-	loadConfig()
-	if db, err = bolt.Open("local.my.db", 0600, &bolt.Options{
-		Timeout:         1 * time.Second,
-		NoGrowSync:      false,
-		ReadOnly:        false,
-		MmapFlags:       0,
-		InitialMmapSize: 0,
-	}); err != nil {
-		glog.Fatalf("open DB %s failed: %s", "my.db", err)
-	}
-	defer db.Close()
+// 	loadConfig()
+// 	if db, err = bolt.Open("local.my.db", 0600, &bolt.Options{
+// 		Timeout:         1 * time.Second,
+// 		NoGrowSync:      false,
+// 		ReadOnly:        false,
+// 		MmapFlags:       0,
+// 		InitialMmapSize: 0,
+// 	}); err != nil {
+// 		glog.Fatalf("open DB %s failed: %s", "my.db", err)
+// 	}
+// 	defer db.Close()
 
-	<-termCh
-	fmt.Println("bye")
-}
+// 	<-termCh
+// 	fmt.Println("bye")
+// }
